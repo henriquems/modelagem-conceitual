42bcaa16e60d12efe2214b2961139484dd8b6926
Henrique Magalhaes Simoes <henriquems@gmail.com>
2018-01-10 19:04:32 -0200

    Estado e Cidade implementados e instanciados

--- src/main/java/com/henrique/cursomc/CursomcApplication.java
+++ src/main/java/com/henrique/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.henrique.cursomc.domain.Categoria;
+import com.henrique.cursomc.domain.Cidade;
+import com.henrique.cursomc.domain.Estado;
 import com.henrique.cursomc.domain.Produto;
 import com.henrique.cursomc.respositories.CategoriaRepository;
+import com.henrique.cursomc.respositories.CidadeRepository;
+import com.henrique.cursomc.respositories.EstadoRepository;
 import com.henrique.cursomc.respositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,9 +50,22 @@ public class CursomcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(c1));
 		p2.getCategorias().addAll(Arrays.asList(c1, c2));
 		p3.getCategorias().addAll(Arrays.asList(c1, c2));
-				
+		
 		categoriaRepository.save(Arrays.asList(c1, c2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
+		
+		Estado e1 = new Estado(null, "Minas Gerais");
+		Estado e2 = new Estado(null, "São Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Uberlandia", e1);
+		Cidade cid2 = new Cidade(null, "São Paulo", e2);
+		Cidade cid3 = new Cidade(null, "Campinas", e2);
+		
+		e1.getCidades().addAll(Arrays.asList(cid1));
+		e2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
+		estadoRepository.save(Arrays.asList(e1, e2));
+		cidadeRepository.save(Arrays.asList(cid1, cid2, cid3));
 	}
 	
 }
